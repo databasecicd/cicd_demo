1b1970c56912ae8e70fe56b442dad0cd1c006b7a
Brian Spendolini <brian.spendolini@oracle.com>
2021-10-13 12:45:44 -1000

    v0.0

--- database/procedure/admin_email_set_procedure.sql
+++
@@ -1,9 +0,0 @@
-
-  CREATE OR REPLACE EDITIONABLE PROCEDURE "TEST"."ADMIN_EMAIL_SET" 
-IS
-BEGIN
-        update trees
-        set submitter_email = 'jeff@thatjeff.com'
-        where submitter_email is null;
-
-end admin_email_set;
